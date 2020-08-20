61b7abb8039a5b9166c0dd9a8db0413a4537247f
Prasan Dhareshwar <prasandhareshwar@gmai.com>
2020-08-20 00:25:44 -0400

    Second Commit: Finished User Authentication

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,15 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
+  const [{ basket, user }] = useStateValue();
+
+  const login = () => {
+    auth.signOut();
+  };
   return (
     <nav className="header">
       <Link to="/">
@@ -21,10 +28,12 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to="/login" className="header__link">
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello,</span>
-            <span className="header__optionLineTwo">Sign In</span>
+        <Link to={!user && "/login"} className="header__link">
+          <div onClick={login} className="header__option">
+            <span className="header__optionLineOne">Hello, {user?.email}</span>
+            <span className="header__optionLineTwo">
+              {user ? `Sign Out` : `Sign In`}
+            </span>
           </div>
         </Link>
 
@@ -45,7 +54,9 @@ function Header() {
         <Link to="/checkout" className="header__link">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
